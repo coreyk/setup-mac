e2b3e709265c56b407d1932459906b0e4db17c30
coreyk <corey@donut.org>
2018-03-31 17:47:06 -0400

    updates

--- setup-mac.sh
+++ setup-mac.sh
@@ -7,10 +7,12 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew update
 
 brew install git
+brew install git-extras
 
 brew install ansible
-brew install bash
-brew install bash-completion2
+# brew install bash
+# brew install bash-completion2
+brew install diff-so-fancy
 brew install go
 brew install jq
 brew install mercurial
@@ -21,6 +23,7 @@ brew install node.js
 brew install putty
 brew install pyenv
 brew install python
+brew install python3
 brew install readline
 brew install redis
 brew install tmux
@@ -28,12 +31,19 @@ brew install tree
 brew install vim
 brew install wget
 brew install zsh
-brew install zsh-completions
+brew install zplug
+brew install csvkit
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 brew tap homebrew/homebrew-php
 brew install php72
 brew install composer
 brew install homebrew/php/php-cs-fixer
+
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
 
 # export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
@@ -47,27 +57,35 @@ brew cask install sublime-text
 
 brew cask install viscosity
 brew cask install tower
+brew cask install p4merge
 
 brew cask install sequel-pro
 brew cask install sqlectron
 brew cask install robo-3t
 brew cask install docker
-brew cask install transmit
+brew cask install transmit4
 brew cask install trailer
 brew cask install 1password
 # brew cask install dropbox
-
+brew cask install gfxcardstatus
 brew cask install onyx
 brew cask install the-unarchiver
 brew cask install gyazo
 brew cask install spectacle
 brew cask install vlc
+brew cask install keka-beta
 
 # password required
 brew cask install avast-security
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install little-snitch
+
+# ftp on High Sierra +
+brew install pure-ftpd
+sudo brew services start pure-ftpd
+pure-pw useradd coreyk -u coreyk -D /Users/coreyk
+pure-pw mkdb
 
 # fun
 brew cask install openemu-experimental
